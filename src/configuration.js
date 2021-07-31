be98257a4bcb64b9896c0bbcad925d9caf0ab778
Ido <idoy23@walla.com>
2021-07-31 15:25:25 +0300

    count api

--- src/configuration.js
+++ src/configuration.js
@@ -2,4 +2,5 @@ export const general = {
   themeColor: "#fff",
   //   themeColor: "#F25278",
   //themeColor: "#bf00ff",
+  countApiKey: "1c29d91d-7ae6-436c-9681-54e9b8970280",
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+
+const sendWebsiteVisitsCounter = () => {
+  try {
+    axios.get("https://api.countapi.xyz/hit/cupidclub.com");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
+    {sendWebsiteVisitsCounter()}
     <App />
   </React.StrictMode>,
   document.getElementById("root")
